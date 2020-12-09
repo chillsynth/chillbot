d4e9dd79e394e89a5f7775bef0c7e95ed65528fe
npgy <me@np.gy>
2020-12-09 21:10:12 +0000

    Groovy can now be used by everyone when a feedback stream is not happening

---
+++ lib/cogs/fb_hosts.py
@@ -0,0 +1,63 @@
+# Import discord dependencies
+from discord import PermissionOverwrite
+from discord.ext.commands import *
+from discord.utils import get
+
+# Other dependencies
+from lib.helpers import url
+
+COG_NAME = "Feedback Hosts"
+
+class CogExt(Cog, name=COG_NAME):
+	def __init__(self, bot):
+		self.bot = bot
+		self.vars_loaded = False
+		self.get_vars()
+
+	@Cog.listener()
+	async def on_ready(self):
+		if not self.bot.ready:
+			self.bot.cogs_ready.ready_up(COG_NAME.lower())
+		await self.bot.statchnl.send(f"`{COG_NAME}` cog loaded")
+
+		self.get_vars()
+
+
+	def get_vars(self):
+		if not self.vars_loaded:
+			try:
+				self.vc_chnl = get(self.bot.guild.voice_channels, name='voice chat')
+				self.fb_chnl = get(self.bot.guild.voice_channels, name='feedback-stream')
+				self.online_role = get(self.bot.guild.roles, name='Online')
+				self.vars_loaded = True
+			except AttributeError:
+				return None
+
+
+	@command()
+	@has_role('Feedback Hosts')
+	async def fb(self, ctx, enable: bool):
+		#Get Groovy member object
+		groovy = self.bot.guild.get_member(234395307759108106)
+		radio_control = get(self.bot.guild.text_channels, name='radio-control')
+		radio_vc = get(self.bot.guild.voice_channels, name='radio')
+
+		# Set permissions
+		await self.fb_chnl.set_permissions(self.online_role, read_messages=enable, connect=enable)
+		await self.vc_chnl.set_permissions(groovy, view_channel=not enable, connect=not enable)
+		await radio_control.set_permissions(groovy, read_messages=not enable)
+		await radio_vc.set_permissions(groovy, view_channel=not enable)
+
+		await ctx.send("Feedback mode is now set to `"+str(enable)+"`")
+
+
+	@command()
+	@has_role('Feedback Hosts')
+	async def fbmove(self, ctx):
+		for member in self.fb_chnl.members:
+				await member.move_to(self.vc_chnl)
+		await ctx.send("Moved all members to <#543601182162157578>!")
+
+
+def setup(bot):
+	bot.add_cog(CogExt(bot))
--- lib/cogs/moderation.py
+++ lib/cogs/moderation.py
@@ -17,29 +17,6 @@ def is_mod(ctx):
 class CogExt(Cog, name=COG_NAME):
 	def __init__(self, bot):
 		self.bot = bot
-		self.vars_loaded = False
-		self.get_vars()
-
-
-	def get_vars(self):
-		if not self.vars_loaded:
-			try:
-				self.vc_chnl = get(self.bot.guild.voice_channels, name='voice chat')
-				self.fb_chnl = get(self.bot.guild.voice_channels, name='feedback-stream')
-				self.online_role = get(self.bot.guild.roles, name='Online')
-				self.vars_loaded = True
-			except AttributeError:
-				return None
-
-
-	@Cog.listener()
-	async def on_ready(self):
-		if not self.bot.ready:
-			self.bot.cogs_ready.ready_up(COG_NAME.lower())
-		await self.bot.statchnl.send(f"`{COG_NAME}` cog loaded")
-
-		self.get_vars()
-
 
 	@Cog.listener()
 	async def on_message(self, msg):
@@ -71,26 +48,6 @@ class CogExt(Cog, name=COG_NAME):
 	@has_any_role(*MODS)
 	async def secret(self, ctx):
 		await ctx.send("poopy butthole!")
-
-
-	@command()
-	@has_role('Feedback Hosts')
-	async def fb(self, ctx, enable: bool):
-		# Set permissions
-		overwrite = PermissionOverwrite()
-		overwrite.read_messages = enable
-		overwrite.connect = enable
-		await self.fb_chnl.set_permissions(self.online_role, overwrite=overwrite)
-
-		await ctx.send("Feedback mode is now set to `"+str(enable)+"`")
-
-
-	@command()
-	@has_role('Feedback Hosts')
-	async def fbmove(self, ctx):
-		for member in self.fb_chnl.members:
-				await member.move_to(self.vc_chnl)
-		await ctx.send("Moved all members to <#543601182162157578>!")
 
 
 def setup(bot):
