0244e69f799830b168ec9fd3acc2333cdd415b47
npgy <me@np.gy>
2021-10-10 15:38:34 -0400

    Added invite creator command

--- lib/cogs/admin.py
+++ lib/cogs/admin.py
@@ -1,4 +1,5 @@
 from discord.ext.commands import *
+import discord
 
 COG_NAME = "Admin"
 
@@ -6,6 +7,7 @@ class CogExt(Cog, name=COG_NAME):
 	def __init__(self, bot):
 		self.bot = bot
 
+	## EVENTS
 
 	@Cog.listener()
 	async def on_ready(self):
@@ -13,6 +15,14 @@ class CogExt(Cog, name=COG_NAME):
 			self.bot.cogs_ready.ready_up(COG_NAME.lower())
 		await self.bot.statchnl.send(f"`{COG_NAME}` cog loaded")
 
+
+	## COMMANDS
+
+	@command()
+	@has_role('**')
+	async def invite(self, ctx, channel: discord.TextChannel, age, uses):
+		inv = await channel.create_invite(max_age=age, max_uses=uses)
+		await ctx.send(f"Successfully created invite `{str(inv)}` for channel <#{channel.id}>")
 
 def setup(bot):
 	bot.add_cog(CogExt(bot))
