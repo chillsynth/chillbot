f46980d1b4e73b1718307e8a1dc20b24a341d327
hurleybirdjr <will.hurley03@gmail.com>
2022-11-03 17:05:41 +0000

    Switched .env to object functionality for event commands

--- bot/events.py
+++ bot/events.py
@@ -9,14 +9,6 @@ import discord
 
 class Events(commands.Cog):
     def __init__(self, bot):
-        # Variables to pre-load
-        self.event_stream = None
-        self.event_stage = None
-        self.event_chat = None
-        self.vc_channel = None
-        self.online_role = None
-        self.event_type = None  # 0 = Stage; 1 = Stream
-
         self.vars_loaded = False
         self.get_vars()
         self.bot = bot
@@ -45,24 +37,25 @@ class Events(commands.Cog):
     async def event(self, interaction: discord.Interaction,
                     event_type: discord.app_commands.Choice[int],
                     enable: bool):
-        self.event_stage = self.bot.get_channel(int(os.getenv("EVENT_STAGE_ID")))
-        self.event_stream = self.bot.get_channel(int(os.getenv("EVENT_STREAM_ID")))
-        self.event_chat = self.bot.get_channel(int(os.getenv("EVENT_CHAT_ID")))
+        test_guild = self.bot.get_guild(int(os.getenv("DEBUG_GUILD_ID")))  # YAY! guild object :catJAM:
+        event_stage = discord.utils.get(test_guild.channels, name="Event Stage")
+        event_stream = discord.utils.get(test_guild.channels, name="Event Stream")
+        event_chat = discord.utils.get(test_guild.channels, name="event-chat")
 
-        self.online_role = discord.utils.get(interaction.guild.roles, name="Online")
+        online_role = discord.utils.get(interaction.guild.roles, name="Online")
         # Set permissions
         if event_type.value == 0:  # STAGE
-            await self.event_stage.set_permissions(self.online_role,
-                                                   connect=enable)
-            await self.event_chat.set_permissions(self.online_role,
-                                                  send_messages=enable)
+            await event_stage.set_permissions(online_role,
+                                              connect=enable)
+            await event_chat.set_permissions(online_role,
+                                             send_messages=enable)
             await interaction.response.send_message(f"Event {event_type.name} mode is now set to '{str(enable)}'")
             pass
         elif event_type.value == 1:  # STREAM
-            await self.event_stream.set_permissions(self.online_role,
-                                                    connect=enable)
-            await self.event_chat.set_permissions(self.online_role,
-                                                  send_messages=enable)
+            await event_stream.set_permissions(online_role,
+                                               connect=enable)
+            await event_chat.set_permissions(online_role,
+                                             send_messages=enable)
             await interaction.response.send_message(f"Event {event_type.name} mode is now set to '{str(enable)}'")
             pass
         else:
@@ -73,22 +66,24 @@ class Events(commands.Cog):
     @app_commands.command(name="emove", description="Moves all members to #Hangout Room VC")
     @app_commands.checks.has_role("Event Host")
     async def eventmove(self, interaction: discord.Interaction):
-        self.vc_channel = self.bot.get_channel(int(os.getenv("PUBLIC_VC_ID")))
-        self.event_stage = self.bot.get_channel(int(os.getenv("EVENT_STAGE_ID")))
-        self.event_stream = self.bot.get_channel(int(os.getenv("EVENT_STREAM_ID")))
-        for member in self.event_stream.members:  # Move all #Event Stream to #Hangout Room
-            await member.move_to(self.vc_channel)
-        for member in self.event_stage.members:  # Move all #Event Stage to #Hangout Room
-            await member.move_to(self.vc_channel)
-        await interaction.response.send_message(f"Moved all members to <#{self.vc_channel}>!")
+        test_guild = self.bot.get_guild(int(os.getenv("DEBUG_GUILD_ID")))  # YAY! guild object :catJAM:
+        vc_channel = discord.utils.get(test_guild.channels, name="Hangout Room")
+        event_stage = discord.utils.get(test_guild.channels, name="Event Stage")
+        event_stream = discord.utils.get(test_guild.channels, name="Event Stream")
+        for member in event_stream.members:  # Move all #Event Stream to #Hangout Room
+            await member.move_to(vc_channel)
+        for member in event_stage.members:  # Move all #Event Stage to #Hangout Room
+            await member.move_to(vc_channel)
+        await interaction.response.send_message(f"Moved all members to <#{vc_channel}>!")
 
     # EVENT HOST ONLY
     @app_commands.command(name="esize", description="Counts all members in the event")
     @app_commands.checks.has_role("Event Host")
     async def eventsize(self, interaction: discord.Interaction):
-        self.event_stream = self.bot.get_channel(int(os.getenv("EVENT_STREAM_ID")))
-        self.event_stage = self.bot.get_channel(int(os.getenv("EVENT_STAGE_ID")))
-        await interaction.response.send_message(f"There are {str(len(self.event_stream.members))} "
+        test_guild = self.bot.get_guild(int(os.getenv("DEBUG_GUILD_ID")))  # YAY! guild object :catJAM:
+        event_stream = discord.utils.get(test_guild.channels, name="Event Stream")
+        # event_stage = discord.utils.get(test_guild.channels, name=" Event Stage")
+        await interaction.response.send_message(f"There are {str(len(event_stream.members))} "
                                                 f"people in the stream!")
 
     # ** ONLY
