341d52295b571289375807d912efaa98f7d680b7
hurleybirdjr <will@hurleybirdjr.com>
2023-08-18 03:18:15 +0100

    fixed the logs

--- bot/__main__.py
+++ bot/__main__.py
@@ -49,7 +49,7 @@ async def main():
     logger.setLevel(logging.INFO)
 
     handler = logging.handlers.RotatingFileHandler(
-        filename='discord.log',
+        filename='logs/discord.log',
         encoding='utf-8',
         maxBytes=32 * 1024 * 1024,  # 32 MiB
         backupCount=5,  # Rotate through 5 files
