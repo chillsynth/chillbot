41d254e92fe0bbe87e2b64540dc41af9e3b86718
Will Hurley <will@hurleybirdjr.com>
2024-05-05 20:36:35 +0100

    Removed DB functions to return to stable

--- bot/extras.py
+++ bot/extras.py
@@ -1,12 +1,9 @@
-import re
 import os
 import logging
-import motor.motor_asyncio
 import discord
 from discord.ext import commands, tasks
 from discord import app_commands  # - USE FOR MANUAL
-from datetime import datetime, timedelta
-from time import mktime
+from datetime import datetime, timedelta, timezone
 import feedparser
 import pytz
 
@@ -15,10 +12,6 @@ class Extras(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        # DB Setup
-        self.client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("DEV_MONGO_URI"))
-        self.db = self.client["_server"]
-
         self.logger = logging.getLogger('discord')
         self.logger.setLevel(logging.INFO)
 
@@ -26,24 +19,10 @@ class Extras(commands.Cog):
         self.logger.info(f"Extras.cog: LOADED!")
         self.logger.info(f"Extras.cog: YouTube Scanner started")
         self.youtube_scan.start()
-        self.resonance_update.start()
 
     async def cog_unload(self):
         # Cancel the task loops when unloaded
         self.youtube_scan.cancel()
-        self.resonance_update.cancel()
-
-    async def cog_app_command_error(self,
-                                    interaction: discord.Interaction,
-                                    error: app_commands.errors.CommandOnCooldown) -> None:
-        cooldown_time = datetime.now() + timedelta(seconds=error.retry_after)
-        cooldown_time_tuple = (cooldown_time.year, cooldown_time.month, cooldown_time.day,
-                               cooldown_time.hour, cooldown_time.minute, cooldown_time.second)
-        print(cooldown_time)
-        await interaction.response.send_message(
-            content=f"Cooldown active! Please retry <t:{int(mktime(datetime(*cooldown_time_tuple).timetuple()))}:R>",
-            delete_after=error.retry_after
-        )
 
     @tasks.loop(minutes=10)
     async def youtube_scan(self):
@@ -64,7 +43,7 @@ class Extras(commands.Cog):
             upload_time = rss_data.entries[0].published
 
             upload_time = datetime.fromisoformat(upload_time)
-            now_time = datetime.utcnow().replace(tzinfo=pytz.utc).replace(microsecond=0)
+            now_time = datetime.now(timezone.utc).replace(tzinfo=pytz.utc).replace(microsecond=0)
             time_diff: timedelta = now_time - upload_time
             self.logger.info(f"Time diff: {time_diff}")
 
@@ -86,98 +65,11 @@ class Extras(commands.Cog):
         self.logger.info(f"Extras.cog: /scan by {interaction.user.id} complete!")
         await interaction.edit_original_response(content="Done!")
 
-    @tasks.loop(minutes=20)  # RATE LIMIT IS 10 minutes ish
-    async def resonance_update(self):
-        # Retrieve leaderboard DB
-        stats_out = None
-        async for result in self.db.stats.find():
-            stats_out = result
-
-        the_guild: discord.Guild = await self.bot.fetch_guild(int(os.getenv("DEV_GUILD_ID")))
-        resonance_channel = await the_guild.fetch_channel(int(os.getenv("DEV_RESONANCE_ID")))
-
-        await resonance_channel.edit(name=f"Resonances: {stats_out['global_resonance_count']}")  # +1 ????
-        self.logger.info(f"Extras.cog: Resonance Update complete!")
-
-    @app_commands.checks.cooldown(1, 120.0, key=None)
-    @app_commands.command(name="leaderboard")
-    async def leaderboard(self, interaction: discord.Interaction):
-        await interaction.response.defer()
-        # Search DB record
-        search_result = None
-        async for result in self.db.stats.find({}):
-            # print(f"Found {new.global_name} under: ObjectID:{result['_id']}")  # DEBUG
-            print(result)
-            search_result = result
-
-        leaderboard_embed = discord.Embed(
-            title="Resonance Leaderboard",
-            colour=0x8080a2,
-            timestamp=datetime.now()
-        )
-
-        leaderboard_embed.add_field(
-            name="1.",
-            value=f"<@{search_result['leaderboard'][0][0]}> --- x {search_result['leaderboard'][0][1]}",
-            inline=False
-        )
-        leaderboard_embed.add_field(
-            name="2.",
-            value=f"<@{search_result['leaderboard'][1][0]}> --- x {search_result['leaderboard'][1][1]}",
-            inline=False
-        )
-        leaderboard_embed.add_field(
-            name="3.",
-            value=f"<@{search_result['leaderboard'][2][0]}> --- x {search_result['leaderboard'][2][1]}",
-            inline=False
-        )
-        leaderboard_embed.add_field(
-            name="4.",
-            value=f"<@{search_result['leaderboard'][3][0]}> --- x {search_result['leaderboard'][3][1]}",
-            inline=False
-        )
-        leaderboard_embed.add_field(
-            name="5.",
-            value=f"<@{search_result['leaderboard'][4][0]}> --- x {search_result['leaderboard'][4][1]}",
-            inline=False
-        )
-
-        leaderboard_embed.set_image(url="https://cdn.discordapp.com/emojis/699652237135183982.webp")
-        leaderboard_embed.set_footer(text="Last Updated")
-
-        await interaction.followup.send(embed=leaderboard_embed)
-
     @commands.Cog.listener()
     async def on_message(self, message):
         if "resonance" in message.content.lower():
             self.logger.info(f"Reacted 'resonance' to message: {message.jump_url}")
             await message.add_reaction(os.getenv("DEV_EMOJI_RESONANCE"))
-
-            # Retrieve leaderboard DB
-            current_stats = None
-            async for result in self.db.stats.find():
-                current_stats = result
-
-            await self.db.members.update_one(
-                {"discord_user_ID": message.author.id},
-                {"$inc": {"server_resonance_count": 1}}
-            )
-
-            await self.db.stats.update_one(
-                {"global_resonance_count": current_stats["global_resonance_count"]},
-                {"$inc": {"global_resonance_count": 1}}
-            )
-
-            current_top_5 = self.db.members.find().sort("server_resonance_count", -1).limit(5)
-            list_cur = await current_top_5.to_list(length=5)
-            resonance_leaderboard = []
-            for i in range(0, 5):
-                resonance_leaderboard.append([list_cur[i]['discord_user_ID'], list_cur[i]['server_resonance_count']])
-
-            await self.db.stats.update_one(
-                {},
-                {"$set": {"leaderboard": resonance_leaderboard}}
-            )
 
         if "electronic gem" in message.content.lower():
             self.logger.info(f"Reacted 'egem' to message: {message.jump_url}")
--- bot/members.py
+++ bot/members.py
@@ -3,9 +3,7 @@ import os
 from discord.ext import commands
 from discord import app_commands
 import discord
-import motor.motor_asyncio
 from datetime import *
-import time
 import logging
 
 
@@ -13,107 +11,11 @@ class Members(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        # DB Setup
-        self.client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("DEV_MONGO_URI"))
-        self.db = self.client["_server"]
-
         self.logger = logging.getLogger('discord')
         self.logger.setLevel(logging.INFO)
 
     async def cog_load(self):
         self.logger.info(f"Members.cog: LOADED!")
-
-    # USERNAME UPDATE
-    @commands.Cog.listener()
-    async def on_user_update(self, old: discord.Member, new: discord.Member):
-        if old.bot:  # User is bot - ignore to prevent TypeError NoneType
-            self.logger.info(f"Skip bot account - {old.global_name} update username to {new.global_name}")
-            pass
-        else:
-            previous_username = f"{old.global_name}"
-            updated_username = f"{new.global_name}"
-            if updated_username is previous_username:  # Username matches - Ignore
-                pass
-            elif updated_username is not previous_username:  # Username has changed
-                self.logger.info(f"{previous_username} changed to {updated_username}")
-
-                # Search DB record
-                key = {"discord_user_ID": new.id}
-                search_result = None
-                async for result in self.db.members.find(key):
-                    # print(f"Found {updated_username} under: ObjectID:{result['_id']}")  # DEBUG
-
-                    search_result = result
-
-                username_updated_value = {
-                    "$set": {
-                        "discord_username": str(f"{updated_username}"),
-                        "past_usernames": {
-                            "username": [
-                                f"{search_result['discord_username']}",
-                                f"{search_result['past_usernames']['username'][0]}",
-                                f"{search_result['past_usernames']['username'][1]}",
-                                f"{search_result['past_usernames']['username'][2]}",
-                                f"{search_result['past_usernames']['username'][3]}"
-                            ],
-                            "user_date_changed": [
-                                int(time.time()),
-                                search_result['past_usernames']['user_date_changed'][0],
-                                search_result['past_usernames']['user_date_changed'][1],
-                                search_result['past_usernames']['user_date_changed'][2],
-                                search_result['past_usernames']['user_date_changed'][3]
-                            ]
-                        }
-                    }
-                }
-
-                db_filter = {"discord_user_ID": search_result["discord_user_ID"]}  # Filter by userID
-                await self.db.members.update_one(db_filter, username_updated_value)
-
-    # NICKNAME UPDATE
-    @commands.Cog.listener()
-    async def on_member_update(self, old: discord.Member, new: discord.Member):
-        if old.bot:  # User is bot - ignore to prevent TypeError NoneType
-            self.logger.info(f"Skip bot account - {old.nick} update nickname to {new.nick}")
-            pass
-        else:
-            if new.nick == old.nick:  # No nickname changes - Ignore
-                pass
-            elif new.nick is not old.nick:  # Nickname has changed
-                self.logger.info(f"{old.nick} changed to {new.nick}")
-
-                # Search DB record
-                key = {"discord_user_ID": new.id}
-                search_result = None
-                async for result in self.db.members.find(key):
-                    # print(f"Found {new.global_name} under: ObjectID:{result['_id']}")  # DEBUG
-
-                    search_result = result
-
-                nickname_updated_value = {
-                    "$set": {
-                        "server_nickname": str(f"{new.nick}"),
-                        "past_nicknames": {
-                            "nickname": [
-                                f"{search_result['server_nickname']}",
-                                f"{search_result['past_nicknames']['nickname'][0]}",
-                                f"{search_result['past_nicknames']['nickname'][1]}",
-                                f"{search_result['past_nicknames']['nickname'][2]}",
-                                f"{search_result['past_nicknames']['nickname'][3]}"
-                            ],
-                            "nick_date_changed": [
-                                int(time.time()),
-                                search_result['past_nicknames']['nick_date_changed'][0],
-                                search_result['past_nicknames']['nick_date_changed'][1],
-                                search_result['past_nicknames']['nick_date_changed'][2],
-                                search_result['past_nicknames']['nick_date_changed'][3]
-                            ]
-                        }
-                    }
-                }
-
-                db_filter = {"discord_user_ID": search_result["discord_user_ID"]}  # Filter by userID
-                await self.db.members.update_one(db_filter, nickname_updated_value)
 
     # MEMBER JOIN PROCESS
     @commands.Cog.listener()
@@ -121,149 +23,6 @@ class Members(commands.Cog):
         if new_member.system:  # Official Discord User
             self.logger.info(f"WOAH! {new_member.global_name} is a system user :)")
             print(f"WOAH! {new_member.global_name} is a system user :)")
-
-        # Search DB record
-        key = {"discord_user_ID": new_member.id}
-        search_result = None
-        async for result in self.db.members.find(key):
-            # print(f"Existing record for {new_member.global_name}"  # DEBUG
-            #      f"found: ObjectID:{result['_id']}")
-
-            search_result = result
-
-        if search_result is not None:  # Database entry found!
-            self.logger.debug(f"Updating existing record for {new_member.global_name}"
-                              f"under: ObjectID:{search_result['_id']}")
-
-            # Update Usernames
-            current_full_username = str(f"{new_member.global_name}")  # For comparing usernames
-            # print(current_full_username)  # DEBUG
-
-            if search_result["discord_username"] == current_full_username:  # Check if username needs updating
-                # print("Username matches!")  # DEBUG
-                pass
-            else:  # Username has changed, update entry with new username
-                # print("Username differs! Updating...")  # DEBUG
-                username_updated_value = {
-                    "$set": {
-                        "discord_username": f"{new_member.global_name}",
-                        "past_usernames": {
-                            "username": [
-                                f"{search_result['discord_username']}",
-                                f"{search_result['past_usernames']['username'][0]}",
-                                f"{search_result['past_usernames']['username'][1]}",
-                                f"{search_result['past_usernames']['username'][2]}",
-                                f"{search_result['past_usernames']['username'][3]}"
-                            ],
-                            "user_date_changed": [
-                                int(search_result['server_leave_date']),
-                                search_result['past_usernames']['user_date_changed'][0],
-                                search_result['past_usernames']['user_date_changed'][1],
-                                search_result['past_usernames']['user_date_changed'][2],
-                                search_result['past_usernames']['user_date_changed'][3]
-                            ]
-                        },
-                        "past_nicknames": {
-                            "nickname": [
-                                f"{search_result['server_nickname']}",
-                                f"{search_result['past_nicknames']['nickname'][0]}",
-                                f"{search_result['past_nicknames']['nickname'][1]}",
-                                f"{search_result['past_nicknames']['nickname'][2]}",
-                                f"{search_result['past_nicknames']['nickname'][3]}"
-                            ],
-                            "nick_date_changed": [
-                                int(search_result['server_leave_date']),
-                                search_result['past_nicknames']['nick_date_changed'][0],
-                                search_result['past_nicknames']['nick_date_changed'][1],
-                                search_result['past_nicknames']['nick_date_changed'][2],
-                                search_result['past_nicknames']['nick_date_changed'][3]
-                            ]
-                        }
-                    }
-                }
-
-                db_filter = {"discord_user_ID": search_result["discord_user_ID"]}  # Filter by userID
-                await self.db.members.update_one(db_filter, username_updated_value)
-
-            # Update Server Join/Leave Date
-            server_updated_values = {
-                "$set": {
-                    "server_join_date": int(time.time()),
-                    "server_leave_date": None,  # Epoch will be filled if left server
-                }
-            }
-
-            query = {"discord_user_ID": search_result["discord_user_ID"]}
-            await self.db.members.update_one(query, server_updated_values)
-
-        elif search_result is None:
-            self.logger.debug(f"No record found for {new_member.global_name}; Creating new entry.")
-
-            # Create new entry in DB
-            creation_date = new_member.created_at
-            # print(str(creation_date))  # DEBUG
-
-            await self.db.members.insert_one(
-                {
-                    "discord_username": str(f"{new_member.global_name}"),
-                    "discord_user_ID": int(new_member.id),
-                    "discord_creation_date": int(creation_date.timestamp()),  # strftime("%d/%m/%Y @ %H:%M:%S")
-                    "server_nickname": str(f"{new_member.name}"),
-                    "server_join_date": int(time.time()),
-                    "server_leave_date": None,  # Epoch will be filled if left server
-                    "server_resonance_count": 0,  # Resonance count yippee reset
-                    "is_booster": bool(False),
-                    "past_usernames": {
-                        "username": ["", "", "", "", ""],
-                        "user_date_changed": ["", "", "", "", ""]
-                    },
-                    "past_nicknames": {
-                        "nickname": ["", "", "", "", ""],
-                        "nick_date_changed": ["", "", "", "", ""]
-                    },
-                    "soundcloud_url": "",
-                    "bandcamp_url": "",
-                    "other_url": ""
-                }
-            )
-        else:
-            self.logger.critical(f"Members.cog: Not sure what I'm supposed to do now... :/")
-
-    # SOCIALS URL MANAGEMENT
-    @app_commands.command(name="socials", description="Edit your music platform social links")
-    async def socials(self, interaction: discord.Interaction.response):
-        """Edit social URLs for feedback stream, etc..."""
-
-        # Search DB record
-        key = {"discord_user_ID": interaction.user.id}
-        search_result = None
-        async for result in self.db.members.find(key):
-            self.logger.debug(f"Found {result['discord_username']} under: ObjectID:{result['_id']}")
-            search_result = result
-
-        current_soundcloud_url: str = ""
-        current_bandcamp_url: str = ""
-        current_other_url: str = ""
-
-        try:
-            current_soundcloud_url = search_result['soundcloud_url']
-        except KeyError:
-            self.logger.debug(f"Members.cog: {interaction.user.global_name} has no SoundCloud URL.")
-            pass
-        try:
-            current_bandcamp_url = search_result['bandcamp_url']
-        except KeyError:
-            self.logger.debug(f"Members.cog: {interaction.user.global_name} has no Bandcamp URL.")
-            pass
-        try:
-            current_bandcamp_url = search_result['other_url']
-        except KeyError:
-            self.logger.debug(f"Members.cog: {interaction.user.global_name} has no Other URL.")
-            pass
-
-        mixed_urls: list = [current_soundcloud_url, current_bandcamp_url, current_other_url]
-
-        await interaction.response.send_modal(Socials(interaction, mixed_urls))
 
     @app_commands.command(name="feedback", description="Submit user feedback")
     async def feedback(self, interaction: discord.Interaction.response):
@@ -274,80 +33,6 @@ class Members(commands.Cog):
     async def report(self, interaction: discord.Interaction.response):
         """Send report to the mod team"""
         await interaction.response.send_modal(UserReport())
-
-
-# SOCIALS MODAL
-class Socials(discord.ui.Modal, title='Social URL Management'):
-    def __init__(self, interaction: discord.Interaction.response, urls: list):
-        # DB Setup
-        self.client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("DEV_MONGO_URI"))  # REPLACE LIVE ENV
-        self.db = self.client["_server"]
-
-        self.logger = logging.getLogger('discord')
-        self.logger.setLevel(logging.INFO)
-
-        super().__init__()
-
-        current_soundcloud_url: str = urls[0]
-        current_bandcamp_url: str = urls[1]
-        current_other_url: str = urls[2]
-
-        self.soundcloud_user_url = discord.ui.TextInput(
-            label="SoundCloud Profile URL",
-            style=discord.TextStyle.short,
-            placeholder="https://soundcloud.com/<your name>",
-            default=f"{current_soundcloud_url}",
-            required=False,
-            min_length=23,  # https://soundcloud.com/
-            max_length=128
-        )
-
-        self.bandcamp_user_url = discord.ui.TextInput(
-            label="Bandcamp Artist URL",
-            style=discord.TextStyle.short,
-            placeholder="https://<your name>.bandcamp.com",
-            default=f"{current_bandcamp_url}",
-            required=False,
-            min_length=21,  # https:// + .bandcamp.com
-            max_length=128
-        )
-
-        self.other_user_url = discord.ui.TextInput(
-            label="Other Artist URL",
-            style=discord.TextStyle.short,
-            placeholder="https://websitehere.com",
-            default=f"{current_other_url}",
-            required=False,
-            min_length=1,  # https:// + .bandcamp.com
-            max_length=128
-        )
-
-        self.add_item(self.soundcloud_user_url)
-        self.add_item(self.bandcamp_user_url)
-        self.add_item(self.other_user_url)
-
-    async def on_submit(self, interaction: discord.Interaction.response):
-        socials_updated_value = {
-            "$set": {
-                "soundcloud_url": str(self.soundcloud_user_url),
-                "bandcamp_url": str(self.bandcamp_user_url),
-                "other_url": str(self.other_user_url)
-            }
-        }
-
-        db_filter = {"discord_user_ID": interaction.user.id}  # Filter by userID
-        await self.db.members.update_one(db_filter, socials_updated_value)
-        self.logger.info(f"Members.cog: {interaction.user.global_name} has updated their Socials()")
-        await interaction.response.send_message(f'Changes have been saved, {interaction.user.name}!', ephemeral=True)
-
-    async def on_timeout(self):
-        self.logger.info(f"Modal was not responded to, continuing...")
-        pass
-
-    async def on_error(self, interaction: discord.Interaction.response, error: Exception) -> None:
-        await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
-        self.logger.error(f"Members.cog: Unable to store {interaction.user.global_name}'s Socials() response")
-        print(error)
 
 
 # USER REPORT MODAL
@@ -426,11 +111,6 @@ class Feedback(discord.ui.Modal, title='Feedback'):
         logger = logging.getLogger('discord')
         logger.setLevel(logging.INFO)
         logger.error(f"{error}")
-
-
-# POLL CREATOR
-
-# POLL DISPLAY
 
 
 async def setup(bot):
