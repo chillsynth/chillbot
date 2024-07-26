06c897efc4fd5edb8e7632d12bac3a7c59624e8e
npgy <me@np.gy>
2024-07-25 20:12:31 -0400

    event

--- internal/bot/handler.go
+++ internal/bot/handler.go
@@ -2,9 +2,9 @@ package bot
 
 import "github.com/bwmarrin/discordgo"
 
-func AddHandler[T any](b *Bot, handler func(dat *T) error) func() {
-	return b.Discord.AddHandler(func(s *discordgo.Session, dat *T) {
-		err := handler(dat)
+func AddHandler[T any](b *Bot, handler func(event *T) error) func() {
+	return b.Discord.AddHandler(func(s *discordgo.Session, event *T) {
+		err := handler(event)
 		if err != nil {
 			b.Logger.LogError(err)
 		}
